d3be69527cdf12ecde592d3876a422a306ebb904
ewa81 <ewa.strzelecka@icloud.com>
2019-09-02 21:49:44 +0200

    calculator resolved task

---
+++ Calculator-master/README.md
@@ -0,0 +1,24 @@
+Gotowe:
+
+- Szablon - czyli plik index.html, cała struktura dokumentu. Proszę zwrócić uwagę, że do elementów dostajemy się przez atrybuty
+np. 'data-caculator-...'..
+
+- Początek skryptu a mianowicie, zadeklarowanie zmiennych potrzebnych do operowania na dokumencie oraz obsłużone 4 wydarzenie na
+kliknięcie.
+
+Do zrobienia:
+- całe style. Kalkulator ma wyglądać tak jak na załączonym obrazku. Nie można zmieniać HTML, tylko CSS.
+
+Jak to zrobić?
+- Po wykonaniu zadania, proszę stworzyć brancha o nazwie 'zadanie', wrzucić kod i stworzyć Pull Requesta.
+
+Jak to ma działać?
+
+1. Wpisujemy dwie liczby w dwa inputy. Inputy nie mogą być puste (jeśli będą puste, proszę wypisać ostrzeżenie).
+2. Jeśli użytkownik dobrze wpisał liczby, musimy sprawdzić czy wprowadzony tekst to rzeczywiście liczby. Jeśli nie,
+musimy je przekonwertować, żeby rzeczywiście były to liczby.
+3. Potem jeśli mamy już liczby, wykonujemy działanie i wypisujemy wynik na ekran.
+
+![Screenshot](IMAGE_1.JPG)
+![Screenshot](IMAGE_2.JPG)
+![Screenshot](IMAGE_3.JPG)
---
+++ Calculator-master/script.js
@@ -0,0 +1,57 @@
+/* jshint esversion: 6 */
+
+const calculatorContainer = document.getElementById('calculatorContainer');
+const numberValue1 = calculatorContainer.querySelector('[data-calculator-value-1]');
+const numberValue2 = calculatorContainer.querySelector('[data-calculator-value-2]');
+const addingOperation = calculatorContainer.querySelector('[data-calculator-operation-adding]');
+const substractiongOperation = calculatorContainer.querySelector('[data-calculator-operation-substraction]');
+const multiplicationOperation = calculatorContainer.querySelector('[data-calculator-operation-multiplication]');
+const divisionOperation = calculatorContainer.querySelector('[data-calculator-operation-division]');
+const resultContainer = calculatorContainer.querySelector('[data-calculator-result-value]');
+const operations = document.getElementsByClassName('calculator__calculate');
+
+addingOperation.addEventListener('click', function () {
+  /** Tu dopisać funkcjonalność dla dodawania */
+
+  const numberValue1 = calculatorContainer.querySelector('[data-calculator-value-1]');
+  const numberValue2 = calculatorContainer.querySelector('[data-calculator-value-2]');
+
+  var sum = 0;
+  sum = number(numberValue1) + number(numberValue2);
+  
+});
+
+substractiongOperation.addEventListener('click', function () {
+  /** Tu dopisać funkcjonalność dla odejmowania */
+
+  const numberValue1 = calculatorContainer.querySelector('[data-calculator-value-1]');
+  const numberValue2 = calculatorContainer.querySelector('[data-calculator-value-2]');
+
+  var sub = 0; 
+  sub = numberValue1 - numberValue2;
+
+  
+});
+
+multiplicationOperation.addEventListener('click', function () {
+  /** Tu dopisać funkcjonalność dla mnożenia */
+
+  const numberValue1 = calculatorContainer.querySelector('[data-calculator-value-1]');
+  const numberValue2 = calculatorContainer.querySelector('[data-calculator-value-2]');
+
+  var multiplication = 0; 
+  multiplication = numberValue1 * numberValue2;
+
+
+});
+
+divisionOperation.addEventListener('click', function () {
+  /** Tu dopisać funkcjonalność dla dzielenia */
+
+  const numberValue1 = calculatorContainer.querySelector('[data-calculator-value-1]');
+  const numberValue2 = calculatorContainer.querySelector('[data-calculator-value-2]');
+
+  var division = 0; 
+  division = umberValue1/umberValue2
+
+});
