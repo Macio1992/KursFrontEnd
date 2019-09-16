c3d149615b99052696e33581fa44d8c68d203e2e
ewa81 <ewa.strzelecka@icloud.com>
2019-09-16 23:16:43 +0200

    Adding file. Exercise: Return the Index of the First Vowel.

---
+++ task1.js
@@ -0,0 +1,20 @@
+
+function firstVowel (str) {
+    console.log(str.search('a'));
+}
+firstVowel('apple');
+
+function firstVowel1 (str) {
+    console.log(str.search('e'));
+}
+firstVowel1('hello');
+
+function firstVowel2 (str) {
+    console.log(str.search('A'));
+}
+firstVowel2('STRAWBERRY');
+
+function firstVowel3 (str) {
+    console.log(str.search('I'));
+}
+firstVowel3('pInEapple');
