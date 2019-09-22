1bc017529da623d0cdf13def65da978cb148e9d4
ewa81 <ewa.strzelecka@icloud.com>
2019-09-22 20:56:01 +0200

    Solved tasks.

---
+++ zadanie5.js
@@ -0,0 +1,16 @@
+/*Maximum Edge of a Triangle
+Create a function that finds the maximum range of a triangles third edge.
+
+Examples
+nextEdge(8, 10) ➞ 17
+
+nextEdge(5, 7) ➞ 11
+
+nextEdge(9, 2) ➞ 10
+Notes
+(side1 + side2) - 1 = maximum range of third edge.
+Triangles have side lengths that are positive integers.*/
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1;
+}
