ae5988c5d652892b12a25c059e504317cf6d6efd
Maix19bep <maix19bep@gmail.com>
2019-09-30 18:27:45 +0200

    exercises

---
+++ ewaTasks.js
@@ -0,0 +1,8 @@
+/*
+  Stwórz tablicę obiektów. Nazwij ją 'tasks'. Przypisz do niej zadania z pliku 'tasks.json'.
+  Stwórz funkcję, która policzy ile zadań ma status IN_PROGRESS i zwróci obiekt w postaci:
+
+  {
+    inProgressTasksCount: <ilosc_zliczonych_zadan>
+  }
+*/
