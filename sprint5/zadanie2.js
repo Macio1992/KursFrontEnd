eddd628a3829f4bf6c6e5e1048caa2c10f0457e0
ewa81 <ewa.strzelecka@icloud.com>
2019-10-02 23:11:09 +0200

    Sprint5: both tasks done.

--- sprint5/zadanie2.js
+++ sprint5/zadanie2.js
@@ -12,3 +12,13 @@ A string containing only whitespaces " " does not count as empty.
 Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.*/
+
+function isEmpty(s) {
+  return s === "";
+}
+
+/*Stowrzyłam funkcję o nazwie isEmpty. Argument tej funkcji nazywa się "s".
+Funkcja ta pobiera argumenty, ktora zwraca true, jeśli
+ciąg znaków jest pusty i false. Funkcja składa się z jednej instrukcji, która
+sygnalizuje zwrot argumentu funkcji zwracanej, jezeli argumenty są równe i tego samego typu.*/
+
--- sprint5/zadanie3.js
+++ sprint5/zadanie3.js
@@ -11,3 +11,10 @@ Notes
 Don't forget to return the result.
 If you get stuck on a challenge, find help in the Resources tab.
 If you're really stuck, unlock solutions in the Solutions tab.*/
+
+function concatName(firstName, lastName) {
+	return lastName.concat(', ', firstName);
+}
+/*Stworzyłam funkcję o nazwie "concatName". Jej argumenty nazywają się firstName, lastName.
+Instrukcja return stanowi treść właściwą tej funkcji. Funkcja ta pobiera argumenty, ktora
+łączy argumenty ciągów do łańcucha i zwraca nowy ciąg.*/
