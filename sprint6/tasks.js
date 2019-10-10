ec9c28bc7aa6112a455197a903cef0430580d954
ewa81 <ewa.strzelecka@icloud.com>
2019-10-10 21:39:38 +0200

    Sprint6: task 1,2,3 done.

--- sprint6/tasks.js
+++ sprint6/tasks.js
@@ -3,14 +3,33 @@
   oblicz sumę liczb z tablicy i wypisz do konsoli tę sumę.
 */
 
+  function addNumber () {
+  let arr = [1,2,4,6,8,9,10];
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++ ) {
+      return sum += arr[i];
+    }
+  }
+  console.log(addNumber());
+
 /*
   Zadanie 2. Stwórz funkcję, która przyjmie tablicę liczb całkowitych i zwróci ich sumę.
 */
 
+  function calcNumber (a , b, c) {
+    return a + b + c;
+  }
+  console.log(calcNumber(3, 5, 6));
 
 /*
   Zadanie 3. Napisz funkcję, która wypisze wszystkie liczby od 0 do 100
 */
+
+function getNumber() {
+  for (let i = 0; i <= 100; i++) {
+    console.log(0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100);
+  }
+}
 
 /*
   Zadanie 4. Napisz funkcję, która przyjmie liczbę całkowitą i wypisze wszystkie liczby
