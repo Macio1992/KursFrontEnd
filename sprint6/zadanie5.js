c9ed1bdedfcb28c02ad6df936bc2723d48e28a1a
ewa81 <ewa.strzelecka@icloud.com>
2019-10-13 14:55:44 +0200

    Sprint6: task5 fixes.

--- sprint6/zadanie5.js
+++ sprint6/zadanie5.js
@@ -3,3 +3,14 @@
   i wypisze wszystkie liczby od `begin` do `end`.
   Jeżeli użytkownik poda większą liczbę `end` niż `begin`, poinformuj go żeby podał odwrotnie
 */
+
+function myFunc (a, b) {
+  if (a < b) {
+    console.log(3,4,5,6,7,8,9,10);
+  } else {
+    if (b > a) {
+      console.log("Zle podałeś");
+    }
+  }
+}
+myFunc(3, 10);
