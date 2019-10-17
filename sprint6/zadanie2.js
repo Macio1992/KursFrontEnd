cfb1b1b623d0372718e301e62ba1af1b4e943e0d
ewa81 <ewa.strzelecka@icloud.com>
2019-10-17 21:06:52 +0200

    Sprint6: fixes name function.

--- sprint6/zadanie2.js
+++ sprint6/zadanie2.js
@@ -2,7 +2,7 @@
   Zadanie 2. Stwórz funkcję, która przyjmie tablicę liczb całkowitych i zwróci ich sumę.
 */
 
-function copyNumber (arr) {
+function sumNumber (arr) {
   let sum = 0;
   for( let i = 0; i < arr.length; i++) {
     sum = sum + arr[i];
