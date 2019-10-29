c73b2948d4306c2cd55828220f662931133c3d3b
ewa81 <ewa.strzelecka@icloud.com>
2019-10-29 22:50:39 +0100

    Sprint6: test space task1.

--- sprint6/zadanie1.js
+++ sprint6/zadanie1.js
@@ -1,7 +1,5 @@
-/*
-Zadanie 1. Stwórz funkcję, zadeklaruj w niej tablicę kilku liczb całkowitych,
-oblicz sumę liczb z tablicy i wypisz do konsoli tę sumę.
-*/
+/*Zadanie 1. Stwórz funkcję, zadeklaruj w niej tablicę kilku liczb całkowitych,
+oblicz sumę liczb z tablicy i wypisz do konsoli tę sumę.*/
 
   function sumNumber() {
     let arr = [1,2,3,4,5,6,7,8,9,10];
