42684f48fd046c720b1455f9f0c13b8339509e71
ewa81 <ewa.strzelecka@icloud.com>
2019-11-02 19:42:51 +0100

    Sprint6: fixes just only task 1,2.

--- sprint6/DO_NOT_TOUCH/zadanie2.js
+++ sprint6/DO_NOT_TOUCH/zadanie2.js
@@ -2,16 +2,16 @@
 Zadanie 2. Stwórz funkcję, która przyjmie tablicę liczb całkowitych i zwróci ich sumę.
 */
 
-function sumNumber(arr) {
+function returnTheSumOfTheNumbersOfAnArray(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    sum = sum + arr[i];
+    sum += arr[i];
   }
   return sum;
 }
 
 /*
-Stworzyłam funkcję o nazwie "sumNumber", która przyjmuje jeden parametr "arr".
+Stworzyłam funkcję o nazwie "returnTheSumOfTheNumbersOfAnArray", która przyjmuje jeden parametr "arr".
 Stworzyłam zmienną lokalną o nazwie sum, do której przypisuje wartość zerową.
 W kazdej iteracji pętli wykonywane jest sumowanie kolejnych elementów tablicy
 liczb całkowitych. Funkcja zwraca wynik sumy.
