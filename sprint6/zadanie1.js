6da59025148e5e1fa02e5a0274eb4a768a5ca97a
ewa81 <ewa.strzelecka@icloud.com>
2019-11-02 11:48:20 +0100

    Sprint6: fixes task.

--- sprint6/zadanie1.js
+++ sprint6/zadanie1.js
@@ -4,13 +4,13 @@ całkowitych, oblicz sumę liczb z tablicy i wypisz do konsoli
 tę sumę.
 */
 
-function sumNumber() {
+function writeTheSumOfTheNumbers() {
   let arr = [1,2,3,4,5,6,7,8,9,10];
-  let suma = 0;
+  let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    suma = suma + arr[i];
+    sum = sum + arr[i];
   }
-  console.log(suma);
+  console.log(sum);
 }
 
 /*
@@ -19,5 +19,5 @@ W funkcji zadeklarowałam zmienne o nazwie arr i sum.
 Do zmiennej "arr" przypisywana jest wartość tablicy liczb naturalnych,
 następnie stworzyłam zmienną lokalną o nazwie "sum",
 do której przypisuje wartość zerową. W kazdej iteracji pętli wykonywane
-jest sumowanie kolejnych elementów tablicy. Funkcja wywołuje wynik sumy.
+jest sumowanie kolejnych elementów tablicy.
 */
