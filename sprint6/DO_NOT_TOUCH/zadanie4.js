79664bc6741e3419532c446b08ca6155ccd973e6
ewa81 <ewa.strzelecka@icloud.com>
2019-11-03 11:28:52 +0100

    Sprint6: fixes second tasks.

--- sprint6/DO_NOT_TOUCH/zadanie4.js
+++ sprint6/DO_NOT_TOUCH/zadanie4.js
@@ -11,16 +11,16 @@ function writeNumber(n) {
     }
   } else {
       console.log('You entered the numbers incorrectly. The number should by smaller than 100.');
-    }
   }
+}
 
 /*
 Stworzyłam funkcję o nazwie "writeNumber", który przyjmuje
 jeden parametr "n". W funkcji pierwsza instrukcja warunkowa if
-sprawdza, czy wartość podanej liczby będzie mniejszy bądz równy od 100,
-funkcja wywołuje przy podaniu róznych liczb do 100.
-W przeciwnym razie, kiedy warunek jest fałszywy, zostanie
-wykonana po instrukcji else, wyświetla informację.
+sprawdza, czy wartość podanej liczby będzie mniejsza bądz równa od 100
+i wypisuje przy podaniu róznych liczb do 100.
+W przeciwnym razie, kiedy warunek jest fałszywy, wykonane
+zostanie polecenie po instrukcji else, wyświetla informację.
 ('You entered the numbers incorrectly. The number should by smaller
 than 100.').
 */
