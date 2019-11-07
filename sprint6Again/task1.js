6ea8f732d9d39b3cebea3a005d5ba44f965efc67
Maix19bep <maix19bep@gmail.com>
2019-11-07 19:56:43 +0100

    sprint6Again: tasks

---
+++ sprint6Again/task1.js
@@ -0,0 +1,7 @@
+/*
+  Stwórz funkcję która przyjmie tablicę liczb całkowitych, odwróci zawartość tej tablicy
+  i zwróci tę odwróconą tablicę. Jeżeli użytkownik poda coś innego niż tablicę, zamiast zwracać
+  odwróconą tablicę, funkcja niech zwraca pustą tablicę.
+ */
+
+/* Encyklopedia: Opisz podstawowe metody string: push, pop, unshift, shift, indexOf, splice */
