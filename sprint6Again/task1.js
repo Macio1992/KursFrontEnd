e91a6e5eafaba744846574ab495786da11002889
ewa81 <ewa.strzelecka@icloud.com>
2019-11-08 23:40:05 +0100

    Sprint6Again: done task.

--- sprint6Again/task1.js
+++ sprint6Again/task1.js
@@ -4,4 +4,18 @@
   odwróconą tablicę, funkcja niech zwraca pustą tablicę.
  */
 
+// First example
+function reverseTable (arr) {
+  let array = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    array.push(arr[i]);
+  }
+  return array;
+}
+
+//Second example
+function reverseTable1() {
+	return [];
+}
+
 /* Encyklopedia: Opisz podstawowe metody string: push, pop, unshift, shift, indexOf, splice */
