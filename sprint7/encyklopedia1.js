cdf3aa53665ebb009d33d50d92977430fcd2b5dd
ewa81 <ewa.strzelecka@icloud.com>
2019-11-12 20:27:16 +0100

    Sprint7: Done encyclopedia, project.

---
+++ sprint7/encyklopedia1.js
@@ -0,0 +1,11 @@
+/*Constructor function
+Słowo kluczowe “this” w kontekście funkcji*/
+
+/*
+Constructor function to zwykła funkcja, pisana z wielkiej litery. Aby utworzyć nowe obiekty na bazie konstruktora skorzystamy ze słowa kluczowego 'new'. Gdy funkcja jest wykonywana za pomocą 'new', wykonuje następujące kroki:
+1. nowy pusty obiekt zostanie utworzony i przypisany do 'this'.
+2. wykonuje się ciało funkcji. 'This' dodaje do niego nowe właściwości.
+3. wartość 'this' zwraca.
+
+Słowo kluczowe 'this' moze uzyskać dostęp do obiektu. Wartością 'this' jest obiekt "przed kropką", uzywany do wywołania funkcji.
+*/
