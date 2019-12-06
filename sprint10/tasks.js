e4768acc1ea533d787cea5fdf5459390caac4190
ewa81 <ewa.strzelecka@icloud.com>
2019-12-06 23:12:05 +0100

    Sprint10: fixes tasks.

--- sprint10/tasks.js
+++ sprint10/tasks.js
@@ -100,16 +100,15 @@ const tasks = [
 ];
 
 function writeAllTitlesAndYears() {
-
   for (let i = 0; i < tasks.length; i++){
     console.log(tasks[i].title + " - " + tasks[i].createdAt.substring(0, 4));
   }
 }
 
 function formatDate(task) {
-  let day = task.substring(0, 4);
-  let month = task.substring(5, 7);
-  let year = task.substring(8, 10);
+  let day = task.createdAt.substring(0, 4);
+  let month = task.createdAt.substring(5, 7);
+  let year = task.createdAt.substring(8, 10);
 
   return year + '/' + month + '/' + day;
 }
