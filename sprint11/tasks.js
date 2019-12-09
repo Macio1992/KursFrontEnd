7b151231723e58f30cc12e50266571458f2c8a75
Maciej Zbierowski <maix19bep@gmail.com>
2019-12-09 14:33:43 +0100

    sprint11: tasks

---
+++ sprint11/tasks.js
@@ -0,0 +1,14 @@
+/*
+    Zadanie 1. Stwórz funkcję, która przyjmuje tablicę zmiennych typu string, i zwraca
+    posortowaną tablice tych zmiennych typu string posortowną według długości zmiennych
+    typu string, rosnąco.
+
+    np. mamy tablicę: ['aaaa', 'a', 'aaa', 'aa'],
+    funkcja ma nam zwrócić tablicę posortowaną rosnąco: ['a', 'aa', 'aaa', 'aaaa']
+
+    np. mamy tablicę ['Google', 'Apple', 'Microsoft']
+    funkcja ma nam zwrócić tablicę posortowaną rosnąco: ['Apple', 'Google', 'Microsoft']
+
+    np. mamy tablice ['Leonardo', 'Michelangelo', 'Raphael', 'Donatello']
+    funkcja ma nam zwrócić tablicę posortowaną rosnąco: ['Raphael', 'Leonardo', 'Donatello', 'Michelangelo']
+*/
