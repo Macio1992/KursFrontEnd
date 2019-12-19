b5400d212be774bd3830fe4ab45cb525f4e37758
Maciej Zbierowski <maix19bep@gmail.com>
2019-12-19 09:38:21 +0100

    sprint11: sortNumbers

---
+++ sprint11/sortNumnbers.js
@@ -0,0 +1,14 @@
+/* Zadanie 1. Stwórz funkcję, która przyjmuje tablicę liczb, i zwraca
+    posortowaną tablice rosnąco. */
+
+function sortNumbersArray(elements) {
+	// Stworz tablice pomocniczą o nazwie sortedElements
+
+	// Znajdź najmniejszy element w tablicy
+
+	// Dopoki tablica nie jest pusta
+		// usun najmniejszy element z tablicy elements
+		// dodaj znaleziony, najmniejszy element do tablicy sortedElements
+
+	// zwroc tablice sortedElements
+}
