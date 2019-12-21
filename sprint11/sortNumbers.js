cf8f5952c9ac566cce5abdb4834fa3e8cf11101b
ewa81 <ewa.strzelecka@icloud.com>
2019-12-21 08:27:37 +0100

    Sprint11: fixes function.

--- sprint11/sortNumbers.js
+++ sprint11/sortNumbers.js
@@ -2,21 +2,14 @@
     posortowaną tablice rosnąco. */
 
 function sortNumbersArray(elements) {
-	// 1. Stworz tablice pomocnicza o nazwie sortedElements
+	let sortedElements = [];
 
-	// 2. Znajdź indeks najmniejszego elementu w tablicy
+	let min = elements[minIndex];
+	let minIndex = 0;
 
 	// 3. Dopoki tablica nie jest pusta
 		// 4. usun najmniejszy element z tablicy elements
 		// 5. dodaj znaleziony, najmniejszy element do tablicy sortedElements
 
-	// 6. zwroc tablice sortedElements
-}
-
-function sortNumbersArray(elements) {
-	let sortedElements = [];
-	let min = elements[0];
-	let minIndex = 0;
-
-
 	return sortedElements;
+}
