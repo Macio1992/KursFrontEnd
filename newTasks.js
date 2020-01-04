1fc51df6dd25367d9874d8a94cea4cc100bcabd5
Maix19bep <maix19bep@gmail.com>
2020-01-04 18:33:53 +0100

    sprint11: change

--- newTasks.js
+++ newTasks.js
@@ -1,14 +1,14 @@
 /*
   Stwórz globalną tablię liczb w tym pliku o nazwie 'numbers'. Dodaj tam liczby:
   [45, 56, 1, -4, 6, 7, 8, 9]
-  Zadanie 1. Stwórz funkcję o nazwie 'findNumber()'. Niech funkcja przyjmie jeden parametr o nazwie 'number'.
+  Zadanie 1. Stwórz funkcję o nazwie 'findNumberIndex()'. Niech funkcja przyjmie jeden parametr o nazwie 'number'.
   Jeżeli 'number' istnieje w tablicy 'numbers', niech funkcja zwróci jej indeks.
   Jeżeli 'number' nie istnieje w tablicy, niecj funkcja zwróci -1.
 
   Np.
-  findNumber(6) powinna zwrócić 4
-  findNumber(9) powinna zwrócić 7
-  findNumber(45) powinna zwrócić 0
-  findNumber(999) powinna zwrócić -1
-  findNumber(99) powinna zwrócić -1
+  findNumberIndex(6) powinna zwrócić 4
+  findNumberIndex(9) powinna zwrócić 7
+  findNumberIndex(45) powinna zwrócić 0
+  findNumberIndex(999) powinna zwrócić -1
+  findNumberIndex(99) powinna zwrócić -1
 */
